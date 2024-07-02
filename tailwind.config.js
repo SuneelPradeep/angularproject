03bc2d73745b6e6fa80c62192f95adffa6388ade
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-07-02 12:19:58 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xs" : "150px",
+        "2xs" : "300px",
+        "xs": "480px",
+        "xxl": "1400px",
+        "3xl": "1920px",
+      },
+    },
+   
   },
   plugins: [],
 }
