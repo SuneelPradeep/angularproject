8f824f82d434117d615c00f24c945069b399f2a3
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-07-04 12:54:52 +0530

    changes overall

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -20,17 +20,23 @@ export class ContentComponent {
   faCheckCircle = faCheckCircle
   items = flowersdata;
   showSort = false;
-  categories: { id : number, name : string, color :string}[] = [{id:1, name:'e-Voucher', color:'Active'},
+  selectedCategories : any=[];
+  categories: { id : number, name : string, color :string}[] = 
+  [{id:1, name:'e-Voucher', color:'Active'},
     {id:2, name:'Products', color:'NActive'},{id:3, name:'Evergreen', color:'NActive'},{id:4, name:'Fashion & Retail', color:'NActive'}]
-
+  
+    initializeCategories (){
+      const uniquecategories = new Set(flowersdata.map(item => item.category))
+      this.categories = Array.from(uniquecategories).map((cat,i) => ({id : i+1, name: cat,color : "NActive" }))
+    }
   numbers : number[] = [...Array(30).keys()].map(i => i + 1);
   toggleSort() {
     this.showSort = !this.showSort;
   }
-
-  resetAll() {
-    this.items = flowersdata;
-  }
+  ngOnInit() {
+    this.initializeCategories()
+    this.updateSelectedCategories()
+ }
 
   sortItems(order: string) {
     if (order === 'asc') {
@@ -40,6 +46,39 @@ export class ContentComponent {
     }
   }
 
+  resetAll() {
+    this.items = flowersdata;
+  }
+  toggleCategorySelection(id: number): void {
+    const category = this.categories.find(cat => cat.id === id);
+    if (category) {
+      category.color = category.color === 'Active' ? 'NActive' : 'Active';
+      this.updateSelectedCategories();
+      this.filterItems()
+    }
+  }
+
+  updateSelectedCategories(): void {
+    this.selectedCategories = this.categories
+      .filter(cat => cat.color === 'Active')
+      .map(cat => cat.name);
+    // this.selectedCategories.push('clear filters');
+  }
+  filterItems(){
+    this.items = flowersdata.filter(item => this.selectedCategories.includes(item.category))
+  }
+   removeFilter(catname : string){
+     const category = this.categories.find(cat => cat.name === catname)
+     if(category){
+      category.color= 'NActive';
+      this.updateSelectedCategories()
+     }
+   }
+
+   clearAllFilters(){
+    this.categories.forEach(cat=> cat.color= "NActive")
+    this.updateSelectedCategories()
+   }
   applySort() {
     this.showSort = false;
   }
